fbbdf719e494207fca341187a35156ee4beaaa9e
John C <jcorrigan322@gmail.com>
2022-08-30 11:31:02 -0400

    typscript webpack setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry:'./src/index.ts',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+    
     module: {
         rules: [
             {
@@ -17,9 +14,17 @@ module.exports = {
                 type: 'asset/resource',
             },
             {
-                test: /\.ts#/i,
+                test: /\.tsx?$/,
                 use: 'ts-loader',
+                exclude: /node_modules/,
             },
         ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+      output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
 };
