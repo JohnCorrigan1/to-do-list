10d43c0699594ac27d99ac35331c9e3ae43b5741
John Corrigan <jcorrigan322@gmail.com>
2022-09-01 15:58:24 -0400

    responsiveness with hamburger menu

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,9 @@ helloWorld = "hi";
 console.log(helloWorld);
 
 const addTask = <HTMLButtonElement> document.getElementById('add-task')!;
-
+const newProjectButton =  <HTMLButtonElement> document.querySelector('.add-project')!
+const hamburger = document.querySelector('.hamburger')!
+const hamburgerNav = <HTMLDivElement> document.querySelector('.hamburger-nav')!
 
 addTask.addEventListener('click', function(){
     console.log("testing")
@@ -18,11 +20,27 @@ addTask.addEventListener('click', function(){
     addTask.disabled = true;
 })
 
-
-const newProjectButton =  <HTMLButtonElement> document.querySelector('.add-project')!
-
 newProjectButton.addEventListener('click', function(){
    console.log("testing 2")
     createProject();
     newProjectButton.disabled = true;
+})
+
+let isActive: boolean = false
+hamburger.addEventListener('click', function(){
+    if(!isActive){
+        hamburgerNav.classList.remove("hamburger-nav")
+        hamburgerNav.classList.add("active")
+        isActive = true
+    }
+    else{
+        hamburgerNav.classList.remove("active")
+        hamburgerNav.classList.add("hamburger-nav")
+        isActive = false
+    }
+    // }
+    // else if(hamburgerNav.style.display = "flex"){
+    //     hamburgerNav.classList.remove("active")
+    //     hamburgerNav.classList.add("hamburger-nav")
+    // }
 })
